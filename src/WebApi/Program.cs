00e5f1b61cf9f3a4f0b4dc7f4c5dc7f9585a00c3
Jackpieking <ledinhdangkhoa10a9@gmail.com>
2024-02-21 14:05:17 +0700

    Master-C2: Init project backend skeleton and modify .gitignore file.

---
+++ src/WebApi/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app
+    .UseHttpsRedirection()
+    .UseRouting();
+
+app.Run();
