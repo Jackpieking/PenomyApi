ba008424ca133f45e0829a2a02df08fec35abd80
nhatxtn <quangnhat26092001@gmail.com>
2024-10-05 22:05:45 +0700

    Update (G8): Remove try catch

--- src/Core/Application/Generic/PenomyAPI.App.FeatG8/G8Handler.cs
+++ src/Core/Application/Generic/PenomyAPI.App.FeatG8/G8Handler.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using PenomyAPI.App.Common;
 using PenomyAPI.Domain.RelationalDb.Entities.ArtworkCreation;
 using PenomyAPI.Domain.RelationalDb.Repositories.Features.Generic;
@@ -17,24 +21,18 @@ namespace PenomyAPI.App.FeatG8
         public async Task<G8Response> ExecuteAsync(G8Request request, CancellationToken ct)
         {
             List<ArtworkChapter> chapters = [];
-            try
+            if (request.Id == 0 || request.StartPage <= 0 || request.PageSize <= 0)
             {
-                if (request.Id == 0 || request.StartPage <= 0 || request.PageSize <= 0)
-                {
-                    return new G8Response { StatusCode = G8ResponseStatusCode.INVALID_REQUEST };
-                }
-                chapters = await _g8Repository.GetArtWorkChapterByIdAsync(
-                    request.Id,
-                    request.StartPage,
-                    request.PageSize,
-                    ct
-                );
+                return new() { StatusCode = G8ResponseStatusCode.INVALID_REQUEST };
             }
-            catch (Exception)
-            {
-                return new G8Response { StatusCode = G8ResponseStatusCode.FAILED };
-            }
-            return new G8Response { Result = chapters, StatusCode = G8ResponseStatusCode.SUCCESS };
+            chapters = await _g8Repository.GetArtWorkChapterByIdAsync(
+                request.Id,
+                request.StartPage,
+                request.PageSize,
+                ct
+            );
+
+            return new() { Result = chapters, StatusCode = G8ResponseStatusCode.SUCCESS };
         }
     }
 }
