f4cc0c2ab379cc44dc295681ba20cde2a2ad2470
Wolrine <volam030102@gmail.com>
2024-10-07 21:22:13 +0700

    Fix register

--- src/External/BuildingBlock/PenomyAPI.BuildingBlock.FeatRegister/FeatureRegistration/G25Reg/G25CountArtViewedReq.cs
+++ src/External/BuildingBlock/PenomyAPI.BuildingBlock.FeatRegister/FeatureRegistration/G25Reg/G25CountArtViewedReq.cs
@@ -1,16 +1,16 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using PenomyAPI.App.G25;
+using PenomyAPI.App.G25.OtherHandlers.NumberArtViewed;
 using PenomyAPI.BuildingBlock.FeatRegister.FeatureRegistration.Common;
 using System;
 
 namespace PenomyAPI.BuildingBlock.FeatRegister.FeatureRegistration.G25Reg
 {
-    internal sealed class G25Reg : FeatureDefinitionRegistration
+    internal sealed class G25CountArtViewedReq : FeatureDefinitionRegistration
     {
-        public override Type FeatRequestType => typeof(G25Request);
+        public override Type FeatRequestType => typeof(G25CountArtViewedRequest);
 
-        public override Type FeatHandlerType => typeof(G25Handler);
+        public override Type FeatHandlerType => typeof(G25CountArtViewedHandler);
 
         public override void AddFeatureDependency(IServiceCollection services, IConfiguration configuration)
         {
