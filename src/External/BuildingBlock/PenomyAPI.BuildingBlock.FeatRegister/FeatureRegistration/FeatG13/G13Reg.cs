05deb23dee6bc1f033bc0b27130746d950209dd0
poollynight <chienthang410@gmail.com>
2024-10-15 14:57:57 +0700

    Done basic flow G13

---
+++ src/External/BuildingBlock/PenomyAPI.BuildingBlock.FeatRegister/FeatureRegistration/FeatG13/G13Reg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PenomyAPI.App.G13;
+using PenomyAPI.BuildingBlock.FeatRegister.FeatureRegistration.Common;
+
+namespace PenomyAPI.BuildingBlock.FeatRegister.FeatureRegistration.G13;
+
+internal sealed class G3Reg : FeatureDefinitionRegistration
+{
+    public override Type FeatRequestType => typeof(G13Request);
+
+    public override Type FeatHandlerType => typeof(G13Handler);
+
+    public override void AddFeatureDependency(
+        IServiceCollection services,
+        IConfiguration configuration
+    ) { }
+}
