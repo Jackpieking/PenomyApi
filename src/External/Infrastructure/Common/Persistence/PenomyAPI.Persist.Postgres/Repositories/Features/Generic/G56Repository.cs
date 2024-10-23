42258727230f25ef5b1ce53e575cdbe9ca929550
poollynight <chienthang410@gmail.com>
2024-10-23 17:39:07 +0700

    Like comment function

---
+++ src/External/Infrastructure/Common/Persistence/PenomyAPI.Persist.Postgres/Repositories/Features/Generic/G56Repository.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using PenomyAPI.App.Common.Models.Common;
+using PenomyAPI.Domain.RelationalDb.Entities.ArtworkCreation;
+using PenomyAPI.Domain.RelationalDb.Repositories.Features.Generic;
+using PenomyAPI.Persist.Postgres.Repositories.Helpers;
+
+namespace PenomyAPI.Persist.Postgres.Repositories.Features.Generic;
+
+public class G56Repository : IG56Repository
+{
+    private readonly DbContext _dbContext;
+    private readonly DbSet<ArtworkComment> _artworkCommentDbSet;
+    private readonly DbSet<UserLikeArtworkComment> _userLikeArtworkCommentDbSet;
+
+    public G56Repository(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _artworkCommentDbSet = dbContext.Set<ArtworkComment>();
+        _userLikeArtworkCommentDbSet = dbContext.Set<UserLikeArtworkComment>();
+    }
+
+    public async Task<long> ExcecuteLikeCommentAsync(
+        long CommentId,
+        long UserId,
+        CancellationToken cancellationToken
+    )
+    {
+        var UserLikeArtworkComment = new UserLikeArtworkComment
+        {
+            UserId = UserId,
+            CommentId = CommentId,
+            LikedAt = DateTime.UtcNow,
+        };
+
+        var result = new Result<long>(0);
+
+        var executionStrategy = RepositoryHelper.CreateExecutionStrategy(_dbContext);
+
+        await executionStrategy.ExecuteAsync(
+            operation: async () =>
+                await LikeCommentAsync(
+                    CommentId,
+                    UserLikeArtworkComment,
+                    cancellationToken: cancellationToken,
+                    result: result
+                )
+        );
+
+        return result.Value;
+    }
+
+    public async Task LikeCommentAsync(
+        long CommentId,
+        UserLikeArtworkComment UserLikeArtworkComment,
+        CancellationToken cancellationToken,
+        Result<long> result
+    )
+    {
+        IDbContextTransaction transaction = null;
+
+        try
+        {
+            transaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
+
+            await _artworkCommentDbSet
+                .Where(c => c.Id == CommentId)
+                .ExecuteUpdateAsync(
+                    c => c.SetProperty(c => c.TotalLikes, c => c.TotalLikes + 1),
+                    cancellationToken: cancellationToken
+                );
+
+            await _userLikeArtworkCommentDbSet.AddAsync(UserLikeArtworkComment);
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            await transaction.CommitAsync(cancellationToken);
+
+            result.Value = 1;
+        }
+        catch
+        {
+            if (transaction != null)
+            {
+                await transaction.RollbackAsync(cancellationToken);
+                result.Value = 2;
+            }
+            await transaction.DisposeAsync();
+
+            result.Value = 3;
+        }
+    }
+}
---
+++ src/External/Presentation/PenomyAPI.Presentation.FastEndpointBasedApi/Endpoints/FeatG56/HttpResponse/G56HttpResponseManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Microsoft.AspNetCore.Http;
+using PenomyAPI.App.FeatG56;
+
+namespace PenomyAPI.Presentation.FastEndpointBasedApi.Endpoints.FeatG56.HttpResponse;
+
+public static class G56HttpResponseManager
+{
+    private static ConcurrentDictionary<
+        G56ResponseStatusCode,
+        Func<G56Request, G56Response, G56HttpResponse>
+    > _dictionary;
+
+    private static void Init()
+    {
+        _dictionary = new();
+
+        // Add each feature status code with its HttpResponse information.
+        _dictionary.TryAdd(
+            key: G56ResponseStatusCode.SUCCESS,
+            value: (_, response) =>
+                new()
+                {
+                    AppCode = $"G56.{G56ResponseStatusCode.SUCCESS}",
+                    HttpCode = StatusCodes.Status200OK,
+                }
+        );
+
+        _dictionary.TryAdd(
+            key: G56ResponseStatusCode.DATABASE_ERROR,
+            value: (_, response) =>
+                new()
+                {
+                    AppCode = $"G56.{G56ResponseStatusCode.DATABASE_ERROR}",
+                    HttpCode = StatusCodes.Status400BadRequest,
+                }
+        );
+        _dictionary.TryAdd(
+            key: G56ResponseStatusCode.NOT_FOUND,
+            value: (_, response) =>
+                new()
+                {
+                    AppCode = $"G56.{G56ResponseStatusCode.NOT_FOUND}",
+                    HttpCode = StatusCodes.Status404NotFound,
+                }
+        );
+        
+    }
+
+    internal static Func<G56Request, G56Response, G56HttpResponse> Resolve(
+        G56ResponseStatusCode statusCode
+    )
+    {
+        if (Equals(objA: _dictionary, objB: default))
+        {
+            Init();
+        }
+
+        return _dictionary[statusCode];
+    }
+}
