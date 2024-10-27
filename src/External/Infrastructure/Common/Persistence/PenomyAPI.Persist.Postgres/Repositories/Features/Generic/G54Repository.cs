54b57c070e9cbef06cc5730f21d15fdc620a342c
poollynight <chienthang410@gmail.com>
2024-10-27 23:17:36 +0700

    G59-Get reply comments- Delete in parentChildComment table when delete reply comment

--- src/External/Infrastructure/Common/Persistence/PenomyAPI.Persist.Postgres/Repositories/Features/Generic/G54Repository.cs
+++ src/External/Infrastructure/Common/Persistence/PenomyAPI.Persist.Postgres/Repositories/Features/Generic/G54Repository.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using PenomyAPI.Domain.RelationalDb.Entities.ArtworkCreation;
 using PenomyAPI.Domain.RelationalDb.Repositories.Features.Generic;
@@ -10,28 +11,40 @@ public class G54Repository : IG54Repository
     private readonly DbContext _dbContext;
     private readonly DbSet<ArtworkComment> _artworkCommentDbSet;
     private readonly DbSet<ArtworkCommentParentChild> _commentParentChildDbSet;
-    private readonly DbSet<ArtworkCommentReference> _commentReferenceDbSet;
 
     public G54Repository(DbContext dbContext)
     {
         _dbContext = dbContext;
         _commentParentChildDbSet = dbContext.Set<ArtworkCommentParentChild>();
         _artworkCommentDbSet = dbContext.Set<ArtworkComment>();
-        _commentReferenceDbSet = dbContext.Set<ArtworkCommentReference>();
     }
 
-    public Task<bool> RemoveCommentAsync(long CommentId)
+    public async Task<bool> RemoveCommentAsync(long CommentId)
     {
         try
         {
             var result = _artworkCommentDbSet.Find(CommentId);
+            var ChildComment = _commentParentChildDbSet
+                .Where(x => x.ChildCommentId == CommentId)
+                .FirstOrDefault();
+
+            if (ChildComment != null)
+            {
+                await _artworkCommentDbSet
+                    .Where(x => x.Id == ChildComment.ParentCommentId)
+                    .ExecuteUpdateAsync(x =>
+                        x.SetProperty(x => x.TotalChildComments, x => x.TotalChildComments - 1)
+                    );
+                _commentParentChildDbSet.Remove(ChildComment);
+            }
+
             _artworkCommentDbSet.Remove(result);
-            _dbContext.SaveChanges();
-            return Task.FromResult(true);
+            await _dbContext.SaveChangesAsync();
+            return true;
         }
         catch
         {
-            return Task.FromResult(false);
+            return false;
         }
     }
 }
