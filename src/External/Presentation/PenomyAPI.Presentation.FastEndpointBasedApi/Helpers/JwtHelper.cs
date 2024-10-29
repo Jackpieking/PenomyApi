58d04c4dadcdbebcb97125086370cda2246446ea
Jackpieking <ledinhdangkhoa10a9@gmail.com>
2024-10-29 22:05:32 +0700

    feat(DevScope): :sparkles: Add access token expire check

--- src/External/Presentation/PenomyAPI.Presentation.FastEndpointBasedApi/Helpers/JwtHelper.cs
+++ src/External/Presentation/PenomyAPI.Presentation.FastEndpointBasedApi/Helpers/JwtHelper.cs
@@ -24,4 +24,25 @@ public static class JwtHelper
             )
             .UtcDateTime;
     }
+
+    /// <summary>
+    ///     Is access token expired.
+    /// </summary>
+    /// <param name="context">
+    ///     The context containe user info.
+    /// </param>
+    /// <returns>
+    ///     True if token is expired.
+    ///     False if token is not expired.
+    /// </returns>
+    public static bool IsAccessTokenExpired(HttpContext context)
+    {
+        var tokenExpireTime = DateTimeOffset
+            .FromUnixTimeSeconds(
+                long.Parse(context.User.FindFirstValue(JwtRegisteredClaimNames.Exp))
+            )
+            .UtcDateTime;
+
+        return tokenExpireTime <= DateTime.UtcNow;
+    }
 }
