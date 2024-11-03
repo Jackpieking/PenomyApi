92826b4e7625342c9bbf693e5aac5cb4c1eb40a5
bradfish29 <duongkhai2904@gmail.com>
2024-11-03 11:07:28 +0700

    fix: Duplication in the BuildingBlock.FeatRegister.csproj

--- src/External/Presentation/PenomyAPI.Presentation.FastEndpointBasedApi/Endpoints/ArtworkCreation/FeatArt8/Art8Endpoint.cs
+++ src/External/Presentation/PenomyAPI.Presentation.FastEndpointBasedApi/Endpoints/ArtworkCreation/FeatArt8/Art8Endpoint.cs
@@ -1,9 +1,11 @@
 ﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using PenomyAPI.App.FeatArt8;
 using PenomyAPI.BuildingBlock.FeatRegister.Features;
 using PenomyAPI.Presentation.FastEndpointBasedApi.Endpoints.ArtworkCreation.FeatArt7.HttpResponseManagers;
 using PenomyAPI.Presentation.FastEndpointBasedApi.Endpoints.ArtworkCreation.FeatArt8.DTOs;
 using PenomyAPI.Presentation.FastEndpointBasedApi.Endpoints.ArtworkCreation.FeatArt8.HttpResponses;
+using PenomyAPI.Presentation.FastEndpointBasedApi.Endpoints.ArtworkCreation.FeatArt8.Middlewares.Authorization;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,20 +16,24 @@ public class Art8Endpoint : Endpoint<Art8RequestDto, Art8HttpResponse>
     public override void Configure()
     {
         Delete("art8/temp-remove/{artworkId}");
-
-        AllowAnonymous();
+        AuthSchemes(JwtBearerDefaults.AuthenticationScheme);
+        PreProcessor<Art8AuthorizationPreProcessor>();
     }
 
     public override async Task<Art8HttpResponse> ExecuteAsync(
         Art8RequestDto requestDto,
         CancellationToken ct)
     {
+        var user = User;
+
         var creatorId = 123456789012345678;
         var request = requestDto.MapToRequest(creatorId);
 
         var featureResponse = await FeatureExtensions.ExecuteAsync<Art8Request, Art8Response>(
             request,
             ct);
+        
+        var t = HttpContext;
 
         var httpResponse = Art8HttpResponseMapper
             .Resolve(featureResponse.AppCode)
