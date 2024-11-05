821457c0791f65fac838354659781fd185538b9c
Wolrine <volam030102@gmail.com>
2024-11-05 21:45:03 +0700

    Change preProcesser

--- src/External/Presentation/PenomyAPI.Presentation.FastEndpointBasedApi/Endpoints/G61/Common/G61StateBag.cs
+++
@@ -1,8 +0,0 @@
-﻿using PenomyAPI.App.G61;
-
-namespace PenomyAPI.Presentation.FastEndpointBasedApi.Endpoints.G61.Common;
-
-public sealed class G61StateBag
-{
-    public G61Request AppRequest { get; set; } = new();
-}
