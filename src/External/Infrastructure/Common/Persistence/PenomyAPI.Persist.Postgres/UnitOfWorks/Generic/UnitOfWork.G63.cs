c2432e954e3f9fa197f0d55d212dcbb2a78e8068
Wolrine <volam030102@gmail.com>
2024-11-09 20:06:37 +0700

    Add feature g63 get followed creators

---
+++ src/External/Infrastructure/Common/Persistence/PenomyAPI.Persist.Postgres/UnitOfWorks/Generic/UnitOfWork.G63.cs
@@ -0,0 +1,19 @@
+﻿using PenomyAPI.Domain.RelationalDb.Repositories.Features.Generic;
+using PenomyAPI.Persist.Postgres.Repositories.Features.Generic;
+
+namespace PenomyAPI.Persist.Postgres.UnitOfWorks;
+
+public sealed partial class UnitOfWork
+{
+    private IG63Repository _G63Repository;
+
+    public IG63Repository G63Repository
+    {
+        get
+        {
+            _G63Repository ??= new G63Repository(_dbContext);
+
+            return _G63Repository;
+        }
+    }
+}
---
+++ src/External/Presentation/PenomyAPI.Presentation.FastEndpointBasedApi/Endpoints/G63/DTOs/G63ResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using PenomyAPI.Domain.RelationalDb.Entities.Generic;
+
+namespace PenomyAPI.Presentation.FastEndpointBasedApi.Endpoints.G63.DTOs;
+
+public class G63ResponseDto
+{
+    public string CreatorId { get; set; }
+    public string NickName { get; set; }
+    public string AvatarUrl { get; set; }
+    public UserGender Gender { get; set; }
+    public string AboutMe { get; set; }
+    public int TotalArtworks { get; set; }
+    public int TotalFollowers { get; set; }
+
+}
