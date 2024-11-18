fea886abc5e09bca8d8df737e678cf3addd302dd
Wolrine <volam030102@gmail.com>
2024-11-18 22:38:06 +0700

    Add feature view joined groups

---
+++ src/Core/Application/SocialMedia/PenomyAPI.App.SM7/SM7Response.cs
@@ -0,0 +1,11 @@
+﻿using PenomyAPI.App.Common;
+using PenomyAPI.Domain.RelationalDb.Entities.SocialMedia;
+
+namespace PenomyAPI.App.SM7;
+
+public class SM7Response : IFeatureResponse
+{
+    public bool IsSuccess { get; set; }
+    public ICollection<SocialGroup> Result { get; set; }
+    public SM7ResponseStatusCode StatusCode { get; set; }
+}
