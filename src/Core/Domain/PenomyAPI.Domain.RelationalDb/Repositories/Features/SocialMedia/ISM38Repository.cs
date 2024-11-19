89f80246aadf3b953a5057ba54084612babd8e88
poollynight <chienthang410@gmail.com>
2024-11-19 21:48:17 +0700

    test commit dev

---
+++ src/Core/Domain/PenomyAPI.Domain.RelationalDb/Repositories/Features/SocialMedia/ISM38Repository.cs
@@ -0,0 +1,9 @@
+﻿using PenomyAPI.Domain.RelationalDb.Entities.SocialMedia;
+using System.Threading.Tasks;
+
+namespace PenomyAPI.Domain.RelationalDb.Repositories.Features.Generic;
+
+public interface ISM38Repository
+{
+    Task<bool> UpdateGroupDetailAsync(long userId, long groupId, string name, string description, string coverPhotoUrl);
+}
