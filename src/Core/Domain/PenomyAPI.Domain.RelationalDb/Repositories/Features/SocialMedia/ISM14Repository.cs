ee0c0e9bd52950a805d901bb8ba5d24777460d5d
poollynight <chienthang410@gmail.com>
2024-12-15 19:25:34 +0700

    HOTFIX sm14 - add remove group post logic

--- src/Core/Domain/PenomyAPI.Domain.RelationalDb/Repositories/Features/SocialMedia/ISM14Repository.cs
+++ src/Core/Domain/PenomyAPI.Domain.RelationalDb/Repositories/Features/SocialMedia/ISM14Repository.cs
@@ -7,6 +7,15 @@ namespace PenomyAPI.Domain.RelationalDb.Repositories.Features.SocialMedia;
 public interface ISM14Repository
 {
     Task<List<long>> GetAttachedFileIdAsync(long userId, CancellationToken cancellationToken);
+    Task<List<long>> GetGroupPostAttachedFileIdAsync(
+        long userId,
+        CancellationToken cancellationToken
+    );
     Task<bool> IsExistUserPostAsync(long id, long userId, CancellationToken cancellationToken);
-    Task<bool> RemoveUserPostAsync(long id, long userId, CancellationToken cancellationToken);
+    Task<bool> RemoveUserPostAsync(
+        long id,
+        long userId,
+        bool isGroupPost,
+        CancellationToken cancellationToken
+    );
 }
--- src/External/Infrastructure/Common/Persistence/PenomyAPI.Persist.Postgres/Repositories/Features/SocialMedia/SM14Repository.cs
+++ src/External/Infrastructure/Common/Persistence/PenomyAPI.Persist.Postgres/Repositories/Features/SocialMedia/SM14Repository.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
 using PenomyAPI.App.Common.Models.Common;
-using PenomyAPI.Domain.RelationalDb.Entities.Generic;
 using PenomyAPI.Domain.RelationalDb.Entities.SocialMedia;
 using PenomyAPI.Domain.RelationalDb.Repositories.Features.SocialMedia;
 using PenomyAPI.Persist.Postgres.Data.DbContexts;
@@ -18,10 +17,18 @@ public class SM14Repository : ISM14Repository
     private readonly DbSet<UserPostAttachedMedia> _attachedMediaContext;
     private readonly AppDbContext _dbContext;
     private readonly DbSet<UserPostLikeStatistic> _likeStatisticsContext;
+    private readonly DbSet<UserLikeUserPost> _userLikeUserPostContext;
     private readonly DbSet<UserPostComment> _userPostCommentContext;
     private readonly DbSet<UserPost> _userPostContext;
     private readonly DbSet<UserPostReport> _userPostReportContext;
-    private readonly DbSet<UserProfile> _userProfileContext;
+
+    // group post
+    private readonly DbSet<GroupPostAttachedMedia> _groupAttachedMediaContext;
+    private readonly DbSet<GroupPostLikeStatistic> _groupLikeStatisticsContext;
+    private readonly DbSet<UserLikeGroupPost> _userLikegroupPostContext;
+    private readonly DbSet<GroupPostComment> _groupPostCommentContext;
+    private readonly DbSet<GroupPost> _groupPostContext;
+    private readonly DbSet<GroupPostReport> _groupPostReportContext;
 
     public SM14Repository(AppDbContext dbContext)
     {
@@ -29,9 +36,16 @@ public class SM14Repository : ISM14Repository
         _userPostContext = _dbContext.Set<UserPost>();
         _attachedMediaContext = _dbContext.Set<UserPostAttachedMedia>();
         _likeStatisticsContext = _dbContext.Set<UserPostLikeStatistic>();
-        _userProfileContext = _dbContext.Set<UserProfile>();
         _userPostCommentContext = _dbContext.Set<UserPostComment>();
         _userPostReportContext = _dbContext.Set<UserPostReport>();
+        _userLikeUserPostContext = _dbContext.Set<UserLikeUserPost>();
+
+        _groupPostContext = _dbContext.Set<GroupPost>();
+        _groupLikeStatisticsContext = _dbContext.Set<GroupPostLikeStatistic>();
+        _userLikegroupPostContext = _dbContext.Set<UserLikeGroupPost>();
+        _groupPostCommentContext = _dbContext.Set<GroupPostComment>();
+        _groupPostReportContext = _dbContext.Set<GroupPostReport>();
+        _groupAttachedMediaContext = _dbContext.Set<GroupPostAttachedMedia>();
     }
 
     public async Task<List<long>> GetAttachedFileIdAsync(
@@ -40,6 +54,17 @@ public class SM14Repository : ISM14Repository
     )
     {
         return await _userPostContext
+            .Where(x => x.CreatedBy == userId)
+            .SelectMany(x => x.AttachedMedias.Select(media => media.Id))
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<List<long>> GetGroupPostAttachedFileIdAsync(
+        long userId,
+        CancellationToken cancellationToken
+    )
+    {
+        return await _groupPostContext
             .Where(x => x.CreatedBy == userId)
             .SelectMany(x => x.AttachedMedias.Select(media => media.Id))
             .ToListAsync(cancellationToken);
@@ -60,6 +85,7 @@ public class SM14Repository : ISM14Repository
     public async Task<bool> RemoveUserPostAsync(
         long id,
         long userId,
+        bool isGroupPost,
         CancellationToken cancellationToken
     )
     {
@@ -67,6 +93,13 @@ public class SM14Repository : ISM14Repository
 
         var executionStrategy = RepositoryHelper.CreateExecutionStrategy(_dbContext);
 
+        if (isGroupPost)
+        {
+            await executionStrategy.ExecuteAsync(
+                async () =>
+                    await InternalRemoveGroupPostByIdAsync(id, userId, cancellationToken, result)
+            );
+        }
         await executionStrategy.ExecuteAsync(
             async () => await InternalRemoveUserPostByIdAsync(id, userId, cancellationToken, result)
         );
@@ -90,6 +123,12 @@ public class SM14Repository : ISM14Repository
                 cancellationToken
             );
 
+            await _userLikeUserPostContext
+                .Where(l => l.PostId == postId)
+                .ExecuteDeleteAsync(cancellationToken);
+            await _likeStatisticsContext
+                .Where(l => l.PostId == postId)
+                .ExecuteDeleteAsync(cancellationToken);
             await _attachedMediaContext
                 .Where(media => media.PostId == postId)
                 .ExecuteDeleteAsync(cancellationToken);
@@ -100,12 +139,63 @@ public class SM14Repository : ISM14Repository
             await _userPostReportContext
                 .Where(rp => rp.PostId == postId)
                 .ExecuteDeleteAsync(cancellationToken);
-            await _likeStatisticsContext
-                .Where(l => l.PostId == postId)
-                .ExecuteDeleteAsync(cancellationToken);
 
             // Set the temporarily removed flag first.
             await _userPostContext
+                .Where(updatedArtwork =>
+                    updatedArtwork.Id == postId && updatedArtwork.CreatedBy == userId
+                )
+                .ExecuteDeleteAsync(cancellationToken);
+
+            await transaction.CommitAsync(cancellationToken);
+
+            result.Value = true;
+        }
+        catch
+        {
+            if (transaction != null)
+            {
+                await transaction.RollbackAsync(cancellationToken);
+                await transaction.DisposeAsync();
+            }
+        }
+    }
+
+    private async Task InternalRemoveGroupPostByIdAsync(
+        long postId,
+        long userId,
+        CancellationToken cancellationToken,
+        Result<bool> result
+    )
+    {
+        IDbContextTransaction transaction = null;
+
+        try
+        {
+            transaction = await RepositoryHelper.CreateTransactionAsync(
+                _dbContext,
+                cancellationToken
+            );
+
+            await _userLikegroupPostContext
+                .Where(l => l.PostId == postId)
+                .ExecuteDeleteAsync(cancellationToken);
+            await _groupLikeStatisticsContext
+                .Where(l => l.PostId == postId)
+                .ExecuteDeleteAsync(cancellationToken);
+            await _groupAttachedMediaContext
+                .Where(media => media.PostId == postId)
+                .ExecuteDeleteAsync(cancellationToken);
+
+            await _groupPostCommentContext
+                .Where(comment => comment.PostId == postId)
+                .ExecuteDeleteAsync(cancellationToken);
+            await _groupPostReportContext
+                .Where(rp => rp.PostId == postId)
+                .ExecuteDeleteAsync(cancellationToken);
+
+            // Set the temporarily removed flag first.
+            await _groupPostContext
                 .Where(updatedArtwork =>
                     updatedArtwork.Id == postId && updatedArtwork.CreatedBy == userId
                 )
